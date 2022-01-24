e06808292cd3cd282f1589a091eb15f507517f93
Arthur Marques <arthur.l2@hotmail.com>
2022-01-24 20:41:21 -0300

    Adicionando CSS Global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import styled from 'styled-components';
+import {GlobalStyle} from './globals/global'
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
+
   );
 }
