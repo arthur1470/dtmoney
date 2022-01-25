114f3972c48a11d64c82badaf98ff78fed6f379b
Arthur Marques <arthur.l2@hotmail.com>
2022-01-25 09:54:23 -0300

    Criacao do componente Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Header } from './components/header'
+import { Header } from './components/Header'
+import { Dashboard } from './components/Dashboard'
 import { GlobalStyle } from './globals/global'
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+    max-width: 1120px;
+    margin: -1rem auto;
+    padding: 2.5rem 1rem;
+`
+
+export const BalanceCard = styled.section`
+    display: flex;
+    flex: 1;
+    background: var(--shape);
+`
